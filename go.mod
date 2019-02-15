0f57fae3ae4f606924d33dfb3e09b72669b1ae5a
Stefan Majewsky <majewsky@gmx.net>
2019-02-15 20:52:50 +0100

    initial implementation
    Did not quite get it to work though.

--- go.mod
+++ go.mod
@@ -1,1 +1,6 @@
 module github.com/majewsky/script2matrix
+
+require (
+	github.com/matrix-org/gomatrix v0.0.0-20190130130140-385f072fe9af
+	github.com/sapcc/go-bits v0.0.0-20190111093410-620b317f22bc
+)
--- main.go
+++ main.go
@@ -18,5 +18,83 @@
 
 package main
 
+import (
+	"os"
+	"os/exec"
+	"regexp"
+	"strings"
+
+	"github.com/matrix-org/gomatrix"
+	"github.com/sapcc/go-bits/logg"
+)
+
+var userIDRx = regexp.MustCompile(`^@(.+):(.+)$`)
+
 func main() {
+	if len(os.Args) < 2 {
+		logg.Fatal("usage: %s <command> [<command-arg>...]", os.Args[0])
+	}
+
+	match := userIDRx.FindStringSubmatch(getenv("MATRIX_USER"))
+	if match == nil {
+		logg.Fatal("malformed environment variable: MATRIX_USER should look like @username:domain.name")
+	}
+
+	//login to Matrix account
+	client, err := gomatrix.NewClient("https://"+match[2], "", "")
+	failIf(err, "create Matrix client")
+	resp, err := client.Login(&gomatrix.ReqLogin{
+		Type:     "m.login.password",
+		User:     match[1],
+		Password: getenv("MATRIX_PASSWORD"),
+	})
+	failIf(err, "login to Matrix account")
+	client.SetCredentials(resp.UserID, resp.AccessToken)
+
+	//read remaining env vars now to detect presence
+	targetRoomID := getenv("MATRIX_TARGET")
+
+	//remove secrets from subcommand environment
+	var commandEnvironment []string
+	for _, env := range os.Environ() {
+		if !strings.HasPrefix(env, "MATRIX_") {
+			commandEnvironment = append(commandEnvironment, env)
+		}
+	}
+
+	//collect output from subcommand
+	cmd := exec.Command(os.Args[1], os.Args[2:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Env = commandEnvironment
+	outputBytes, err := cmd.CombinedOutput()
+	output := string(outputBytes)
+	if err != nil {
+		if !strings.HasSuffix(output, "\n") {
+			output += "\n"
+		}
+		output += err.Error()
+	}
+
+	_, err = client.SendMessageEvent(targetRoomID, "m.room.message", gomatrix.TextMessage{
+		MsgType: "m.text",
+		Body:    output,
+	})
+	failIf(err, "send message to "+targetRoomID)
+
+	_, err = client.Logout()
+	failIf(err, "logout from Matrix account")
+}
+
+func getenv(key string) string {
+	val := os.Getenv(key)
+	if val == "" {
+		logg.Fatal("missing environment variable: " + key)
+	}
+	return val
+}
+
+func failIf(err error, msg string) {
+	if err != nil {
+		logg.Fatal("%s: %s", msg, err.Error())
+	}
 }
